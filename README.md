0ebf15fe05cc7662e88b8cf689983b172b3d9ce6
COCOUVI <cocouvialexandro74@gmail.com>
2026-09-09 08:11:31 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# cicdfast
+
+## Installation
+```
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+## Lancer le serveur
+```
+uvicorn app.main:app --reload
+```
---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="Mon API", version="0.1.0")
+
+
+@app.get("/")
+def read_root():
+    return {"message": "API en ligne"}
+
+@app.get("/mycicd")
+def read_root():
+    return {"message": "ci et cd sont prets"}
